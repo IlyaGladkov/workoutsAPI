201a56829eb661e6a949dbc0781767d826d7e182
iliyagladkov <glagkov.iliya@gmail.com>
2025-03-31 15:00:19 +0300

    get all workouts

---
+++ src/controllers/workoutController.js
@@ -0,0 +1,57 @@
+const workoutServices = require('../services/workoutService')
+
+const getAllWorkouts = (req, res) => {
+    const allWorkouts = workoutServices.getAllWorkouts()
+    res.send({ status: 'OK', data: allWorkouts })
+}
+
+const getOneWorkout = (req, res) => {
+    const workout = workoutServices.getOneWorkout()
+    res.send('Get an existing workout')
+}
+
+const createNewWorkout = (req, res) => {
+    const body = req
+
+    if (
+        !body.name ||
+        !body.mode ||
+        !body.equipment ||
+        !body.exercises ||
+        !body.trainerTips
+    ) {
+      return
+    }
+
+    const newWorkout = {
+        name: body.name,
+        mode: body.mode,
+        equipment: body.equipment,
+        exercises: body.exercises,
+        trainerTips: body.trainerTips,
+    }
+
+    const createdWorkout = workoutServices.createNewWorkout(newWorkout)
+    res.status(201).send({
+        status: 'OK',
+        data: createdWorkout
+    })
+}
+
+const updateOneWorkout = (req, res) => {
+    const updateWorkout = workoutServices.updateOneWorkout()
+    res.send('Update an existing workout')
+}
+
+const deleteOneWorkout = (req, res) => {
+    const deleteWorkout = workoutServices.deleteOneWorkout()
+    res.send('Delete an existing workout')
+}
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+}
