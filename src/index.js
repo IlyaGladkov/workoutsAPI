e7c717f86a47f4e2e5047919b3c4269889a3ea82
iliyagladkov <glagkov.iliya@gmail.com>
2025-04-01 12:46:24 +0300

    data caching

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const apicache = require('apicache')
 
 const v1workoutRouter = require('./v1/routes/workoutRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const cache = apicache.middleware
 
 app.use(bodyParser.json())
+app.use(cache('2 minutes'))
 app.use('/api/v1/workouts', v1workoutRouter)
 
 app.listen(PORT, () => {
